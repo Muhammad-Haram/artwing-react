54307d9bdaee4a1e76699352f2e99473bceab877
Muhammad Haram <haramh643@gmail.com>
2024-03-11 08:37:38 +0000

    add some tailwind in testimonial

--- src/Components/Home/testimonial.jsx
+++ src/Components/Home/testimonial.jsx
@@ -26,42 +26,197 @@ export default function App() {
             >
                 <SwiperSlide>
                     <div>
-                        <div>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
-                            <i className="fa-solid fa-star"></i>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
                         </div>
-                        <p>I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
                             store & CRO, resulting in a 60% sales increase and a 45% boost in customer
                             engagement. Their dedication and seamless communication made it feel like a
                             partnership. Outstanding!</p>
                         <div className='flex'>
                             <img src={person1} alt="" />
-                            <div>
-                                <h4>Adam</h4>
-                                <p>Product Manager</p>
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
                             </div>
                         </div>
                     </div>
                 </SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
-                <SwiperSlide>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus, vitae culpa accusantium ad reprehenderit ea eaque nulla aut cupiditate officiis impedit maiores ducimus nobis veritatis unde architecto? Cumque, optio deleniti.
-                    Voluptates earum necessitatibus fuga esse! Et est vitae quaerat dolorum voluptate soluta quibusdam quam odit. Quaerat nihil deleniti sapiente saepe! Aliquid illo soluta deserunt voluptas in autem iure incidunt aliquam!</SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">Hats off to Artwing Alpha for their SEO expertise! Our online visibility saw a
+                            phenomenal boost, resulting in a 60% increase in website traffic and a 40% rise
+                            in search engine rankings. What sets them apart is their collaborative approach
+                            – it felt like we were part of one unstoppable team. Can't thank them enough!
+                        </p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide><SwiperSlide>
+                    <div>
+                        <div className="text-start mb-3">
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                            <i className="fa-solid fa-star text-[#FCE383] text-[12px] m-2"></i>
+                        </div>
+                        <p className="text-[18px] text-[#6D787D] text-start mb-5">I'm blown away by the Alpha team’s e-commerce solutions. They revamped our online
+                            store & CRO, resulting in a 60% sales increase and a 45% boost in customer
+                            engagement. Their dedication and seamless communication made it feel like a
+                            partnership. Outstanding!</p>
+                        <div className='flex'>
+                            <img src={person1} alt="" />
+                            <div className='flex-row text-left text-white ml-4'>
+                                <h4 className='text-base font-semibold'>Adam</h4>
+                                <p className='text-[#6D787D] text-[16px]'>Product Manager</p>
+                            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
             </Swiper>
         </>
     );
